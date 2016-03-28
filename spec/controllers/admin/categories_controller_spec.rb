f7e334c5c5780c6f3269d02942436337e62c45cc
Brittany <BrittanyKohler@gmail.com>
2016-03-28 15:13:41 -0700

    Adding rspec test for new method in admin categories controller

--- spec/controllers/admin/categories_controller_spec.rb
+++ spec/controllers/admin/categories_controller_spec.rb
@@ -16,6 +16,17 @@ describe Admin::CategoriesController do
     assert_response :redirect, :action => 'index'
   end
 
+  describe "test_new" do
+    before(:each) do
+      get :new
+    end
+
+    it 'should render template new' do
+      get "new"
+      expect(subject).to render_template :new
+    end
+  end
+
   describe "test_edit" do
     before(:each) do
       get :edit, :id => Factory(:category).id
