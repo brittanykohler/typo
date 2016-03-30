647a53f3bb72a5a0bd9d5abca71c843be9e9837a
Brittany <BrittanyKohler@gmail.com>
2016-03-30 16:31:26 -0700

    removing commented out code

--- app/models/article.rb
+++ app/models/article.rb
@@ -97,7 +97,6 @@ class Article < Content
   def merge_with(other_article_id)
     other_article = Article.find(other_article_id)
     # Merge body
-    # current_body = self.body
     self.body = self.body + other_article.body
     self.save
     # Merge comments
@@ -105,6 +104,7 @@ class Article < Content
       comment.article_id = self.id
       comment.save
     end
+    # Delete other article
     other_article.delete
     return self
   end
