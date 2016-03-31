29f959efdad806ae07f04a34903b6bf0f47361c1
Brittany <BrittanyKohler@gmail.com>
2016-03-31 15:27:30 -0700

    Updating merge_with method to not iterate through each comment when merging comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -98,12 +98,9 @@ class Article < Content
     other_article = Article.find(other_article_id)
     # Merge body
     self.body = self.body + other_article.body
-    self.save
     # Merge comments
-    other_article.comments.each do |comment|
-      comment.article_id = self.id
-      comment.save
-    end
+    self.comments = self.comments + other_article.comments
+    self.save
     # Delete other article
     other_article.delete
     return self
